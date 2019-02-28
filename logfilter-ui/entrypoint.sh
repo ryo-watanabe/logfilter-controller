95e8b83c6076603662ebcb4d9a41fbddf53d67d3
watanabe-ryou <fj3817ia@aa.jp.fujitsu.com>
2019-02-28 18:18:52 +0900

    ES/grafana resources. UI basic auth.

--- logfilter-ui/entrypoint.sh
+++ logfilter-ui/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 kubectl config set-cluster cluster --server=https://kubernetes.default.svc
 kubectl config set-cluster cluster --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
@@ -7,5 +8,11 @@ kubectl config set-context context --cluster=cluster --user=apache --namespace=$
 kubectl config use-context context
 cp /root/.kube/config /kubecfg
 chown apache /kubecfg
+
+if [ "$UI_USER" != "" ] && [ "$UI_PASSWORD" != "" ] ; then
+  htpasswd -cb /.htpasswd $UI_USER $UI_PASSWORD
+else
+  rm -f /var/www/localhost/htdocs/.htaccess
+fi
 
 exec httpd -DFOREGROUND
